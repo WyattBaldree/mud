4a5a884c436e15cf34278aae720a268f5307c8d0
deniella <36905047+deniella@users.noreply.github.com>
2020-04-27 16:38:49 -0400

    css scroll fixing

--- public/js/client.js
+++ public/js/client.js
@@ -1,12 +1,14 @@
 console.log('Client-side code running');
 
 var socket = io();
+let enterPress = false;
 
 function handleCommand(e){
 	if(e.code == "Enter"){
 		let commandLine = document.getElementById("commandLine");
 	    socket.emit('command', commandLine.value);
 	    commandLine.value = "";
+	    enterPress = true;
 	}
 }
 
@@ -24,5 +26,10 @@ commandLine.onkeypress = handleCommand;
 socket.on("chat message", function(msg){
 	let li = document.createElement("li")
 	li.append(msg);
-	document.getElementById("messageLog").append(li);
+	document.getElementById("messageLog").prepend(li);
+	if(enterPress){
+		let messageLog = document.getElementById("messageLog");
+		messageLog.scrollTop = messageLog.scrollHeight - messageLog.clientHeight;
+		enterPress = false;
+	}
 });
