c95e2b3342831ec5bbe92cbbf2750a11a487e410
WyattBaldree <wyattmbaldree@gmail.com>
2020-04-29 09:37:24 -0400

    updloading a sql database export.

---
+++ mud.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 29, 2020 at 03:36 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `characters`
+--
+
+CREATE TABLE `characters` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL DEFAULT 'New Character',
+  `class` varchar(255) NOT NULL DEFAULT 'Default Class',
+  `currentRoom` int(255) NOT NULL DEFAULT -1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `id` int(11) NOT NULL,
+  `roomName` varchar(255) NOT NULL DEFAULT 'newRoom',
+  `roomDescription` varchar(1024) NOT NULL DEFAULT 'An empty room.',
+  `north` int(11) NOT NULL DEFAULT -1,
+  `east` int(11) DEFAULT -1,
+  `south` int(11) NOT NULL DEFAULT -1,
+  `west` int(11) NOT NULL DEFAULT -1,
+  `playerList` varchar(1024) NOT NULL DEFAULT '{"playerList":[]}'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `characters` varchar(1024) NOT NULL DEFAULT '-1,-1,-1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `characters`) VALUES
+(1, 'Wyatt', 'werter', '{\"characters\":[])'),
+(2, 'hello', 'werter', '{\"characters\":[])'),
+(3, 'Deniella', 'rusty', '{\"characters\":[])'),
+(4, 'BroBro', 'Bro', '{\"characters\":[])'),
+(5, 'h', 'jeff', '{\"characters\":[])'),
+(6, 'j', 'j', '{\"characters\":[])'),
+(7, 'k', 'k', '{\"characters\":[])'),
+(8, '', 'h', '{\"characters\":[])'),
+(9, 'Nintin', 'password', '{\"characters\":[])'),
+(10, 'g', '', '{\"characters\":[])');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `characters`
+--
+ALTER TABLE `characters`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `characters`
+--
+ALTER TABLE `characters`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
