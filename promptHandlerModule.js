494476bb4a5502a3c168be42489337b923c8b7f7
WyattBaldree <wyattmbaldree@gmail.com>
2020-04-28 23:39:00 -0400

    Made some style changes

--- promptHandlerModule.js
+++ promptHandlerModule.js
@@ -18,14 +18,19 @@ exports.handlePromptReply = function(io, socket, promptType, promptReply){
 function regAccount(io, socket, promptType, promptReply){
 	switch(promptReply.toLowerCase()){
 		case "login":
+			break;
+		case "r":
 		case "register":
 			socket.temp = {
 				username: "",
 				password: ""
 				};
+			socket.emit('chat message', 'Registration started.');
 			socket.emit('prompt request', 'regUsername', "Register your username: ");
 			break;
-		default: 
+		default:
+			socket.emit('chat message', 'Invalid choice. Try typing register or login.');
+			socket.emit('prompt request', "accountInitialization", "Login or Register?");
 	}
 }
 
@@ -42,6 +47,7 @@ function regUsername(io, socket, promptType, promptReply){
 function regUsernameCallback(result, username, socket){
 	if(result.length <= 0){ //if promptReply is a valid username
 		socket.temp.username = username;
+		socket.emit('chat message', 'Username, '+ username + ', accepted.');
 		socket.emit('prompt request', 'regPassword', "Register your password: ");
 	}else{
 		socket.emit('chat message', "Username taken.")
@@ -57,6 +63,7 @@ function regPassword(io, socket, promptType, promptReply){
 	if(true){ //if promptReply is a valid password
 		socket.temp.password = promptReply;
 		exports.mySqlModule.insert("users", "username, password", socket.temp.username, socket.temp.password);
+		socket.emit('chat message', 'Password accepted.');
 		socket.emit('chat message', "Account successfully created.")
 		socket.emit('prompt request', 'accountInitialization', "Login or Register?");
 	}else{
