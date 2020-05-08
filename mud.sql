dab8f64ae89f8762049a24110e0e375a78f94d0d
WyattBaldree <wyattmbaldree@gmail.com>
2020-05-07 21:43:05 -0400

    woops forgot this

--- mud.sql
+++ mud.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 07, 2020 at 10:01 PM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Generation Time: May 08, 2020 at 03:42 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -42,7 +42,7 @@ CREATE TABLE `characters` (
 --
 
 INSERT INTO `characters` (`id`, `characters_firstName`, `characters_lastName`, `characters_race`, `characters_class`, `characters_currentRoom`) VALUES
-(0, 'Default Character', '', 0, 0, 1),
+(0, 'Default Character', '', 0, 0, 6),
 (1, 'Fendryn', 'Telvanni', 1, 1, 1),
 (6, 'Jeff', 'Barus', 0, 0, 1),
 (7, 'deathwing', 'wing', 1, 0, 1),
@@ -116,12 +116,12 @@ CREATE TABLE `rooms` (
 
 INSERT INTO `rooms` (`id`, `rooms_name`, `rooms_description`, `rooms_north`, `rooms_east`, `rooms_south`, `rooms_west`, `rooms_playerList`) VALUES
 (0, 'CenterRoom', 'You stand in the center of a large dimly lit room. You see an exit to the north, south, east, and west.', 1, 3, 5, 7, ''),
-(1, 'NorthRoom', 'You enter the town square but you do not hear anything or anyone.', -1, 2, 0, 8, ''),
+(1, 'NorthRoom', 'You enter the town square but you do not hear anything or anyone.', -1, 2, 0, 8, '1,7,'),
 (2, 'NorthEastCorner', 'You enter an empty room that is faintly warm.', -1, -1, 3, 1, ''),
 (3, 'EastRoom', 'You enter a hot room and start to sweat.', 2, -1, 4, 0, ''),
 (4, 'SouthEastCorner', 'You enter a room that is faintly warm with a sweet smell.', 3, -1, -1, 5, ''),
 (5, 'SouthRoom', 'You enter a room but do not see anything.', 0, 4, -1, 6, ''),
-(6, 'SouthWestCorner', 'You enter a room with a slight breeze.', 7, 5, -1, -1, ''),
+(6, 'SouthWestCorner', 'You enter a room with a slight breeze.', 7, 5, -1, -1, '0,'),
 (7, 'WestRoom', 'An empty room. It\'s very windy.', 8, 0, 6, -1, ''),
 (8, 'NorthWestCorner', 'An empty room with no apparent life.', -1, 1, 7, -1, '');
 
@@ -135,19 +135,21 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `users_username` varchar(255) NOT NULL,
   `users_password` varchar(255) NOT NULL,
-  `users_characters` varchar(1024) NOT NULL DEFAULT '-1,-1,-1'
+  `users_characters` varchar(1024) NOT NULL DEFAULT '-1,-1,-1',
+  `users_online` tinyint(1) NOT NULL DEFAULT 0,
+  `users_spamTokens` int(11) NOT NULL DEFAULT 10
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `users_username`, `users_password`, `users_characters`) VALUES
-(0, 'Default', 'Default1!', '0,6,-1'),
-(1, 'Banjoman64', 'werter@5S', '1,-1,-1'),
-(14, 'den', 'Rusty1!', '10,-1,7'),
-(20, 'Banjo', 'werter@5S', '-1,-1,-1'),
-(21, 'Banjoman65', 'werter@5S', '9,-1,-1');
+INSERT INTO `users` (`id`, `users_username`, `users_password`, `users_characters`, `users_online`, `users_spamTokens`) VALUES
+(0, 'Default', 'Default1!', '0,6,-1', 1, 10),
+(1, 'Banjoman64', 'werter@5S', '1,-1,-1', 0, 10),
+(14, 'den', 'Rusty1!', '10,-1,7', 1, 10),
+(20, 'Banjo', 'werter@5S', '-1,-1,-1', 0, 10),
+(21, 'Banjoman65', 'werter@5S', '9,-1,-1', 0, 10);
 
 --
 -- Indexes for dumped tables
